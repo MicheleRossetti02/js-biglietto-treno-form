f69d2cde56908624f1f4a6b3ce83e49174c95f29
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-10-07 17:51:29 +0200

    prezzo

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,35 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 
+//infomrazioni principali
+
 const fullName = prompt("Indica il tuo nome completo");
 console.log(fullName);
 const kmScelti = prompt ("Quanti kilometri vuole percorrere?");
+console.log(kmScelti);
+const age = prompt ("Quanti anni hai?");
+console.log(age);
+
+
 const euro ="€";
 const price = 0.21;
 const totalPrice = (kmScelti * price);
+console.log(totalPrice);
+
+if(age<=18){
+    const sconto = totalPrice * 20 / 100 ;
+    const prezzoFinale = totalPrice - sconto;
+
+} else if (age>=65){
+    const sconto = totalPrice * 40 / 100 ;
+    const prezzoFinale = totalPrice - sconto;
+
+}
+
 const buttonGenerator = document.getElementById("genera");
 
 buttonGenerator.addEventListener ("click", function() {
-
+        // Codice da eseguire qui
+        console.log('hai cliccato sulla colonna');
+      
 })
